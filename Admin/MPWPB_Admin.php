d6d53b68959229e06dda171def806c8cc48e9e01
Shahadat <raselsha@gmail.com>
2024-09-27 09:23:31 +0600

    refactor: old file removed

--- Admin/MPWPB_Admin.php
+++ Admin/MPWPB_Admin.php
@@ -34,7 +34,6 @@
 				require_once MPWPB_PLUGIN_DIR . '/Admin/settings/MPWPB_Price_Settings.php';
 				require_once MPWPB_PLUGIN_DIR . '/Admin/settings/MPWPB_Extra_service_Settings.php';
 				require_once MPWPB_PLUGIN_DIR . '/Admin/settings/MPWPB_Faq_Settings.php';
-				//require_once MPWPB_PLUGIN_DIR . '/Admin/settings/MPWPB_FAQ_Settings.php';
 				//****************Woocommerce Checkout*********************** */
 				require_once MPWPB_PLUGIN_DIR . '/Admin/MPWPB_Wc_Checkout_Settings.php';
 				require_once MPWPB_PLUGIN_DIR . '/Admin/MPWPB_Wc_Checkout_Fields.php';
