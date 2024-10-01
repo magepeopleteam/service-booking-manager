e1e11f67ad281679476e1530733d1ebc69967763
Shahadat <raselsha@gmail.com>
2024-10-01 12:16:38 +0600

    feat: poup up cart updated

--- templates/themes/static.php
+++ templates/themes/static.php
@@ -46,7 +46,7 @@
                     </div>
                 </div>
                 <div class="mpPopup mpStyle" data-popup="#mpwpb_static_popup">
-                    <div class="popupMainArea max_900">
+                    <div class="popupMainArea">
                         <div class="popupHeader">
                             <h5><?php echo esc_html( $title ); ?></h5>
                             <p><?php echo esc_html( $sub_title ); ?></p>
@@ -54,7 +54,7 @@
                         </div>
                         <div class="popupBody">
                             <div class="mpwpb-popup-content">
-                                <div class="mpwpb_static_service">
+                                <div class="service-items">
                                     <div class="all_service_area ">
                                         <?php include(MPWPB_Function::template_path('registration/category_selection.php')); ?>
                                         <?php include(MPWPB_Function::template_path('registration/service_selection.php')); ?>
@@ -63,7 +63,7 @@
                                     <?php include(MPWPB_Function::template_path('registration/date_time_select.php')); ?>
                                     <div class="mpwpb_order_proceed_area"></div>
                                 </div>
-                                <div class="mpwpb_summary_area_left">
+                                <div class="service-cart">
                                     <div class="mpwpb_summary_area_left_content">
                                         <?php if (sizeof($all_category) > 0) { ?>
                                             <div class="mpwpb_summary_item" data-category>
