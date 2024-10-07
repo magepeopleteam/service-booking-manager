7ab3addba46d1f5bce79e28d281f69434c988352
Shahadat <raselsha@gmail.com>
2024-10-07 09:33:49 +0600

    fix: statice data condition fixed

--- Frontend/MPWPB_Static_Template.php
+++ Frontend/MPWPB_Static_Template.php
@@ -41,21 +41,33 @@ if(! class_exists('MPWPB_Static_Template') ){
         }
 
         public function show_service_nav() {
+            $service_overview_status = get_post_meta(get_the_ID(),'mpwpb_service_overview_status',true);
+            $faq_status = get_post_meta(get_the_ID(),'mpwpb_faq_active',true);
+            $service_details_status = get_post_meta(get_the_ID(),'mpwpb_service_details_status',true);
+            $reviews_status = 'off';
             ?>
             <nav>
                 <ul>
-                    <li>
-                        <a href="#service-overview"><?php _e('Overview','service-booking-manager') ?></a>
-                    </li>
-                    <li>
-                        <a href="#service-faq"><?php _e('FAQ','service-booking-manager') ?></a>
-                    </li>
+                    <?php if($service_overview_status === 'on'): ?>
+                        <li>
+                            <a href="#service-overview"><?php _e('Overview','service-booking-manager') ?></a>
+                        </li>
+                    <?php endif; ?>
+                    <?php if($faq_status === 'on'): ?>
+                        <li>
+                            <a href="#service-faq"><?php _e('FAQ','service-booking-manager') ?></a>
+                        </li>
+                    <?php endif; ?>
+                    <?php if($service_details_status === 'on'): ?>
                     <li>
                         <a href="#service-details"><?php _e('Details','service-booking-manager') ?></a>
                     </li>
+                    <?php endif; ?>
+                    <?php if($reviews_status === 'on'): ?>
                     <li>
                         <a href="#service-reviews"><?php _e('Reviews','service-booking-manager') ?></a>
                     </li>
+                    <?php endif; ?>
                 </ul>
             </nav>
             <?php
@@ -64,21 +76,24 @@ if(! class_exists('MPWPB_Static_Template') ){
         public function show_service_overview() {
             $service_overview_status = get_post_meta(get_the_ID(),'mpwpb_service_overview_status',true);
             $service_overview_content = get_post_meta(get_the_ID(),'mpwpb_service_overview_content',true);
+            if($service_overview_status === 'on'):
             ?>
                 <section id="service-overview">
                     <h2><?php _e('Servie Overview','service-booking-manager'); ?></h2>
-                    <?php echo $service_overview_status === 'on' ? wp_kses_post($service_overview_content) : '';?>
+                    <?php echo wp_kses_post($service_overview_content);?>
                 </section>
             <?php
+            endif;
         }
 
         public function show_service_faq() {
-            
+            $mpwpb_faq = get_post_meta(get_the_ID(),'mpwpb_faq',true);
+            $faq_status = get_post_meta(get_the_ID(),'mpwpb_faq_active',true);
+            if($faq_status=='on'):
             ?>
             <section id="service-faq">
                 <h2><?php _e('Servie FAQ','service-booking-manager'); ?></h2>
                 <?php 
-                        $mpwpb_faq = get_post_meta(get_the_ID(),'mpwpb_faq',true);
                         if( ! empty($mpwpb_faq)){
                             foreach ($mpwpb_faq as $key => $value) {
                                 $this->show_faq_data($value['title'],$value['content']);
@@ -87,6 +102,7 @@ if(! class_exists('MPWPB_Static_Template') ){
                     ?>
             </section>
             <?php
+            endif;
         }
 
         public function show_faq_data($title,$content){
@@ -105,23 +121,28 @@ if(! class_exists('MPWPB_Static_Template') ){
         public function show_service_details() {
             $service_details_status = get_post_meta(get_the_ID(),'mpwpb_service_details_status',true);
             $service_details_content = get_post_meta(get_the_ID(),'mpwpb_service_details_content',true);
+            if($service_details_status === 'on'):
             ?>
             <section id="service-details">
                 <h2><?php _e('Servie Details','service-booking-manager'); ?></h2>
                 <?php 
-                    echo $service_details_status === 'on' ? wp_kses_post($service_details_content) : '';
+                    echo wp_kses_post($service_details_content);
                 ?>
             </section>
             <?php
+            endif;
         }
 
         public function show_service_reviews() {
+            $review_status = 'off';
+            if($review_status === 'on'):
             ?>
             <section id="service-reviews">
                 <h2><?php _e('Servie Reviews','service-booking-manager'); ?></h2>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo alias distinctio fugiat corrupti. In tempore voluptatibus consequuntur! Magnam asperiores tempora accusamus, quasi doloremque cumque, ipsa dolores tenetur possimus, voluptas quo dolorem? Quo vero rerum id, ullam, sapiente harum temporibus corrupti aliquam perferendis, error quisquam ipsum hic blanditiis aperiam ea nihil non saepe sint exercitationem! Consectetur, error quidem consequuntur labore explicabo quisquam sapiente vel architecto? Consequatur tenetur eum vero error repellat neque optio, atque dignissimos sed, suscipit perferendis. Quis ipsum asperiores dolor laudantium officiis tempore tempora, doloribus quae quo ea veniam, odit nobis ullam! Beatae facere iusto corrupti! Voluptas, vel corporis?
             </section>
             <?php
+            endif;
         }
 
     }
