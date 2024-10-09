545bd4de1560bf54ee17f3acf153f610ff0f0204
Shahadat <raselsha@gmail.com>
2024-10-09 14:30:05 +0600

    style: default template

--- templates/themes/default.php
+++ templates/themes/default.php
@@ -50,107 +50,99 @@
                 <!-- dispaly service Reviews section using this hook -->
                 <?php do_action('mpwpb_service_reviews'); ?>
         </div>
-        <div class="sidebar">
-            <div class="mpwpb_static_theme">
-
-                <div class="mpStyle">
-                    <div>
-                        <div>
-                            <h5><?php echo esc_html( $title ); ?></h5>
-                            <p><?php echo esc_html( $sub_title ); ?></p>
-                        </div>
-                        <div class="popupBody">
-                            <div class="mpwpb-popup-content">
-                                <div class="service-items">
-                                    <div class="all_service_area ">
-                                        <?php include(MPWPB_Function::template_path('registration/category_selection.php')); ?>
-                                        <?php include(MPWPB_Function::template_path('registration/service_selection.php')); ?>
-                                        <?php include(MPWPB_Function::template_path('registration/extra_services.php')); ?>
-                                    </div>
-                                    <?php include(MPWPB_Function::template_path('registration/date_time_select.php')); ?>
-                                    <div class="mpwpb_order_proceed_area"></div>
-                                </div>
-                                <div class="service-cart">
-                                    <div class="mpwpb_summary_area_left_content">
-                                        <?php if (sizeof($all_category) > 0) { ?>
-                                            <div class="mpwpb_summary_item" data-category>
-                                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                                <h6></h6>
-                                            </div>
-                                        <?php } ?>
-
-                                        <?php if (sizeof($all_sub_category) > 0) { ?>
-                                            <div class="mpwpb_summary_item" data-sub-category>
-                                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                                <h6></h6>
-                                            </div>
-                                        <?php } ?>
-
-                                        <?php if (sizeof($all_service_list) > 0) { ?>
-                                            <?php foreach ($all_service_list as $service_list) { ?>
-                                                <div class="mpwpb_summary_item" data-service="<?php echo esc_attr($service_list['service']); ?>" data-service-category="<?php echo esc_attr($service_list['category']); ?>" data-service-sub-category="<?php echo esc_attr($service_list['sub_category']); ?>">
-                                                    <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                                    <div class="flexWrap justifyBetween">
-                                                        <h6 class="mR_xs"><?php echo esc_html($service_list['service']); ?></h6>
-                                                        <p><span class="textTheme">x1</span>&nbsp;|&nbsp; <span class="textTheme service_price"><?php echo MP_Global_Function::wc_price($post_id, $service_list['price']); ?></span></p>
-                                                    </div>
-                                                </div>
-                                            <?php } ?>
-                                        <?php } ?>
-                                        <?php
-                                            if (sizeof($extra_services) > 0) {
-                                                foreach ($extra_services as $group_service) {
-                                                    $group_service_name = array_key_exists('group_service', $group_service) ? $group_service['group_service'] : '';
-                                                    $ex_service_infos = array_key_exists('group_service_info', $group_service) ? $group_service['group_service_info'] : [];
-                                                    if (sizeof($ex_service_infos) > 0) {
-                                                        foreach ($ex_service_infos as $ex_service_info) {
-                                                            $ex_service_price = array_key_exists('price', $ex_service_info) ? $ex_service_info['price'] : 0;
-                                                            ?>
-                                                            <div class="mpwpb_summary_item" data-extra-service="<?php echo esc_attr($ex_service_info['name']); ?>">
-                                                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                                                <div class="flexWrap justifyBetween">
-                                                                    <h6 class="mR_xs">
-                                                                        <?php
-                                                                            echo esc_html($ex_service_info['name']);
-                                                                            if ($group_service_name) {
-                                                                                echo esc_html('(&nbsp;' . $group_service_name . '&nbsp;)');
-                                                                            }
-                                                                        ?>
-                                                                    </h6>
-                                                                    <p>
-                                                                        <span class="textTheme ex_service_qty">x1</span>&nbsp;|&nbsp;
-                                                                        <span class="textTheme"><?php echo MP_Global_Function::wc_price($post_id, $ex_service_price); ?></span>
-                                                                    </p>
-                                                                </div>
-                                                            </div>
-                                                            <?php
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        ?>
-                                        <div class="mpwpb_summary_item" data-date>
-                                            <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                            <h6></h6>
-                                        </div>
-                                        <div class="mpwpb_summary_item">
-                                            <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
-                                            <div class="flexWrap justifyBetween">
-                                                <h5 class="mR_xs"><?php esc_html_e('Total :', 'service-booking-manager'); ?></h5>
-                                                <h5><span class="mpwpb_total_bill textTheme"><?php echo MP_Global_Function::wc_price($post_id, 0); ?></span></h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+        <div class="sidebar mpStyle">
+            <div class="header">
+                <h5><?php echo esc_html( $title ); ?></h5>
+                <p><?php echo esc_html( $sub_title ); ?></p>
+            </div>
+            <div class="content">
+                <div class="service-items">
+                    <div class="all_service_area ">
+                        <?php include(MPWPB_Function::template_path('registration/category_selection.php')); ?>
+                        <?php include(MPWPB_Function::template_path('registration/service_selection.php')); ?>
+                        <?php include(MPWPB_Function::template_path('registration/extra_services.php')); ?>
+                    </div>
+                    <?php include(MPWPB_Function::template_path('registration/date_time_select.php')); ?>
+                    <div class="mpwpb_order_proceed_area"></div>
+                </div>
+                <div class="service-cart">
+                    <div class="mpwpb_summary_area_left_content">
+                        <?php if (sizeof($all_category) > 0) { ?>
+                            <div class="mpwpb_summary_item" data-category>
+                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                                <h6></h6>
                             </div>
+                        <?php } ?>
+
+                        <?php if (sizeof($all_sub_category) > 0) { ?>
+                            <div class="mpwpb_summary_item" data-sub-category>
+                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                                <h6></h6>
+                            </div>
+                        <?php } ?>
+
+                        <?php if (sizeof($all_service_list) > 0) { ?>
+                            <?php foreach ($all_service_list as $service_list) { ?>
+                                <div class="mpwpb_summary_item" data-service="<?php echo esc_attr($service_list['service']); ?>" data-service-category="<?php echo esc_attr($service_list['category']); ?>" data-service-sub-category="<?php echo esc_attr($service_list['sub_category']); ?>">
+                                    <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                                    <div class="flexWrap justifyBetween">
+                                        <h6 class="mR_xs"><?php echo esc_html($service_list['service']); ?></h6>
+                                        <p><span class="textTheme">x1</span>&nbsp;|&nbsp; <span class="textTheme service_price"><?php echo MP_Global_Function::wc_price($post_id, $service_list['price']); ?></span></p>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        <?php } ?>
+                        <?php
+                            if (sizeof($extra_services) > 0) {
+                                foreach ($extra_services as $group_service) {
+                                    $group_service_name = array_key_exists('group_service', $group_service) ? $group_service['group_service'] : '';
+                                    $ex_service_infos = array_key_exists('group_service_info', $group_service) ? $group_service['group_service_info'] : [];
+                                    if (sizeof($ex_service_infos) > 0) {
+                                        foreach ($ex_service_infos as $ex_service_info) {
+                                            $ex_service_price = array_key_exists('price', $ex_service_info) ? $ex_service_info['price'] : 0;
+                                            ?>
+                                            <div class="mpwpb_summary_item" data-extra-service="<?php echo esc_attr($ex_service_info['name']); ?>">
+                                                <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                                                <div class="flexWrap justifyBetween">
+                                                    <h6 class="mR_xs">
+                                                        <?php
+                                                            echo esc_html($ex_service_info['name']);
+                                                            if ($group_service_name) {
+                                                                echo esc_html('(&nbsp;' . $group_service_name . '&nbsp;)');
+                                                            }
+                                                        ?>
+                                                    </h6>
+                                                    <p>
+                                                        <span class="textTheme ex_service_qty">x1</span>&nbsp;|&nbsp;
+                                                        <span class="textTheme"><?php echo MP_Global_Function::wc_price($post_id, $ex_service_price); ?></span>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <?php
+                                        }
+                                    }
+                                }
+                            }
+                        ?>
+                        <div class="mpwpb_summary_item" data-date>
+                            <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                            <h6></h6>
                         </div>
-                        <div class="popupFooter _justifyBetween">
-                            <?php include(MPWPB_Function::template_path('registration/next_service.php')); ?>
-                            <?php include(MPWPB_Function::template_path('registration/next_date_time.php')); ?>
+                        <div class="mpwpb_summary_item">
+                            <span class="fas fa-check mpwpb_item_check _circleIcon_xs"></span>
+                            <div class="flexWrap justifyBetween">
+                                <h5 class="mR_xs"><?php esc_html_e('Total :', 'service-booking-manager'); ?></h5>
+                                <h5><span class="mpwpb_total_bill textTheme"><?php echo MP_Global_Function::wc_price($post_id, 0); ?></span></h5>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="footer _justifyBetween">
+                <?php include(MPWPB_Function::template_path('registration/next_service.php')); ?>
+                <?php include(MPWPB_Function::template_path('registration/next_date_time.php')); ?>
+            </div>
+            
         </div>
     </main>
 
