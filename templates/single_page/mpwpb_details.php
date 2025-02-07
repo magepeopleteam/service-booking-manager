21c3d72df9395c9965d737b8c39453b98e3177c7
MagePeople,Inc <48548664+magepeopleteam@users.noreply.github.com>
2025-02-07 09:58:45 +0600

    Update mpwpb_details.php

--- templates/single_page/mpwpb_details.php
+++ templates/single_page/mpwpb_details.php
@@ -6,8 +6,33 @@
 	if (!defined('ABSPATH')) {
 		die;
 	} // Cannot access pages directly.
-	get_header();
-	the_post();
+<?php if ( wp_is_block_theme() ) {  ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php
+	$block_content = do_blocks( '
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+		<!-- wp:post-content /-->
+		</div>
+		<!-- /wp:group -->'
+ 	);
+    wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div class="wp-site-blocks">
+<header class="wp-block-template-part site-header">
+    <?php block_header_area(); ?>
+</header>
+</div>
+<?php
+} else {
+    get_header();	
+    the_post();
+}
 	do_action('mpwpb_single_page_before_wrapper');
 	if (post_password_required()) {
 		echo wp_kses_post(get_the_password_form()); // WPCS: XSS ok.
@@ -30,4 +55,15 @@
 		include_once(MPWPB_Function::details_template_path($post_id));
 	}
 	do_action('mpwpb_single_page_after_wrapper');
-	get_footer();
+if ( wp_is_block_theme() ) {
+// Code for block themes goes here.
+?>
+<footer class="wp-block-template-part">
+    <?php block_footer_area(); ?>
+</footer>
+<?php wp_footer(); ?>
+</body>    
+<?php
+} else {
+    get_footer();
+}
