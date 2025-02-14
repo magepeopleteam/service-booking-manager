3be8a4741b2c6f6908b20abb5141f8decc66e15d
Shahadat <raselsha@gmail.com>
2025-02-14 10:12:52 +0600

    fix: category name displaying

--- Admin/settings/Category.php
+++ Admin/settings/Category.php
@@ -275,14 +275,19 @@
 				return $service_category;
 			}
 			public function get_category_by_id($post_id,$cat_id) {
-				$service_category = get_post_meta($post_id, 'mpwpb_category_service', true);
-				$service_category = !empty($service_category) ? $service_category : [];
-				return $service_category[$cat_id];
+				if($cat_id!=''){
+					$service_category = get_post_meta($post_id, 'mpwpb_category_service', true);
+					$service_category = !empty($service_category) ? $service_category : [];
+					return $service_category[$cat_id];
+				}
 			}
 			public function get_sub_category_by_id($post_id,$cat_id) {
-				$sub_category = get_post_meta($post_id, 'mpwpb_sub_category_service', true);
-				$sub_category = !empty($sub_category) ? $sub_category : [];
-				return $sub_category[$cat_id];
+				if($cat_id!=''){
+					$sub_category = get_post_meta($post_id, 'mpwpb_sub_category_service', true);
+					$sub_category = !empty($sub_category) ? $sub_category : [];
+					return $sub_category[$cat_id];
+				}
+				
 			}
 			public function get_sub_categories($post_id) {
 				$sub_category = get_post_meta($post_id, 'mpwpb_sub_category_service', true);
