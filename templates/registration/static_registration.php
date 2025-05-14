5bdc083ad196073a0d950f1d6f4332fd6277c0ab
sjrubel10 <rubelcuet10@gmail.com>
2025-05-14 11:56:45 +0600

    icon missing in static fixed

--- templates/registration/static_registration.php
+++ templates/registration/static_registration.php
@@ -37,7 +37,10 @@
                                     <div class="mpwpb_show_all_category_holder mpwpb_selected_category" id="mpwpb_show_all_category" >
                                         <?php esc_html_e( 'All Category', 'service-booking-manager' );?> <i class="fa-solid fa-arrow-right"></i>
                                     </div>
-                                    <div class="mpwpb_selected_category_text mpwpb_category_selected_item mpActive mpwpb_selected_category" data-category=""></div>
+                                    <div class="mpwpb_text_icon_holder">
+                                        <div class="mpwpb_selected_category_text mpwpb_category_selected_item mpActive mpwpb_selected_category" data-category=""></div>
+                                        <div class="mpwpb_arrow_icon_holder" style="display: none"><i class="fa-solid fa-arrow-right"></i></div>
+                                    </div>
                                     <div class="mpwpb_selected_sub_category_text mpwpb_selected_category" data-category='' data-sub-category=""></div>
                                 </div>
 
