d64fd84646b71e992e2d623fe44f12426084746b
Shahadat <raselsha@gmail.com>
2025-11-10 09:47:20 +0600

    style: meta icon update

--- Admin/MPWPB_Extended_Settings.php
+++ Admin/MPWPB_Extended_Settings.php
@@ -33,7 +33,7 @@ if (!class_exists('MPWPB_Extended_Settings')) {
             if ( is_plugin_active('service-booking-manager-pro/MPWPB_Plugin_Pro.php') ) {
 			?>
 			<li data-tabs-target="#mpwpb_recurring_booking">
-				<i class="fas fa-redo pe-1"></i><?php esc_html_e('Recurring Booking', 'service-booking-manager'); ?>
+				<i class="mi mi-loop-square"></i><?php esc_html_e('Recurring Booking', 'service-booking-manager'); ?>
 			</li>
 			<!--<li data-tabs-target="#mpwpb_waiting_list">
 				<i class="fas fa-user-clock pe-1"></i><?php /*esc_html_e('Waiting List', 'service-booking-manager'); */?>
